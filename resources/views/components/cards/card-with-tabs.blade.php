e0afa2d382b2743b2ed0f9046d43a498d548a1e4
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2021-07-14 11:53:12 -0500

    component for card with tabs

---
+++ resources/views/components/cards/card-with-tabs.blade.php
@@ -0,0 +1,10 @@
+<div class="card card-dark">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      {{ $tabs }}
+    </ul>
+  </div>
+  <div class="card-body">
+    {{ $slot }}
+  </div>
+</div>
---
+++ resources/views/components/cards/tab.blade.php
@@ -0,0 +1,3 @@
+<li class="nav-item">
+  <a {{ $attributes->class(['nav-link'])->merge(['href' => '#']) }}>{{ $slot }}</a>
+</li>
